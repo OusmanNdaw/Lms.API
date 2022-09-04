0394b0bf635f3567b2a4a9cf24af7079dd4987a9
OusmanNdaw <105175511+OusmanNdaw@users.noreply.github.com>
2022-09-04 02:28:19 +0200

    Uppdate Program.cs

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers(opt =>.ReturnHttpNotAcceptable = true)
-    .AddNewtonsoftJson()
+builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
+   // .AddNewtonsoftJson
     .AddXmlDataContractSerializerFormatters();
 
 var app = builder.Build();
